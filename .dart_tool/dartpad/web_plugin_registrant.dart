a1be9591bf24bde5bb41d724fef17cfa72212c84
shayann07 <shayankhan17353@gmail.com>
2025-07-29 23:31:12 +0500

    App Done!

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,28 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:device_info_plus/src/device_info_plus_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_database_web/firebase_database_web.dart';
+import 'package:flutter_inappwebview_web/web/main.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  DeviceInfoPlusWebPlugin.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseDatabaseWeb.registerWith(registrar);
+  InAppWebViewFlutterPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
